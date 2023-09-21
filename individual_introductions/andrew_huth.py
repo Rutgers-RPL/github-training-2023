ebda43e24d63ff0029a027f759048d31a642da7e
Andrew Huth <andrew.huth@rutgers.edu>
2023-09-21 16:19:06 -0400

    Github Training Additions

---
+++ individual_introductions/andrew_huth.py
@@ -0,0 +1,3 @@
+def intro():
+    print('Hi! My name is Andrew Huth, I am a sophomore in Aerospace Engineering\n' + 
+          'and a Telemetry Lead for RPL!\n')
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ sys.path.append('./individual_introductions') # or this 👉👈
 
 # Put all imports below this line
 from individual_introductions import shivam_patel # import the file containing your individual introduction blurb
+from individual_introductions import andrew_huth
 
 def main():
     
     shivam_patel.intro() # call your intro function
+    andrew_huth.intro()
 
 
 if __name__ == '__main__':
