140406998ee8154cb47a3156a9cf347d8e0e6f87
Chance REYES <chance.reyes@rutgers.edu>
2023-09-21 21:36:35 -0400

    GitHub Training Additions & Changes

---
+++ individual_introductions/chance_reyes.py
@@ -0,0 +1,4 @@
+def intro():
+    print('Hey, my name is Chance Reyes. I am a freshman taking ECE.' +
+          '\nI don\'t have much experience in rocketry but I have been working on projects with microcontrollers' +
+          '\nI hope to have a great time here and have lots to learn from you guys!\n')
