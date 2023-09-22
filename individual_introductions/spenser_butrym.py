f3f6c7fe464ecd6761c049068ddd1a58078f42e3
Spenser Butrym <spenserb8@gmail.com>
2023-09-21 21:12:42 -0400

    Github Training Additions & Changes

---
+++ individual_introductions/spenser_butrym.py
@@ -0,0 +1,5 @@
+def intro():
+
+	print('Hi, my name is Spenser Butrym. I\'m a 2nd year mechanical engineering major.' + 
+		'\nI\'ve been part of AIAA and RPL since I was a freshman. It\'s my second year here. '
+		'I\'m looking forward to working in telemetry this year')
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ sys.path.append('./individual_introductions') # or this 👉👈
 # Put all imports below this line
 from individual_introductions import shivam_patel # import the file containing your individual introduction blurb
 from individual_introductions import andrew_huth
+from individual_introductions import spenser_butrym 
 
 def main():
     
     shivam_patel.intro() # call your intro function
     andrew_huth.intro()
-
+    spenser_butrym.intro()
 
 if __name__ == '__main__':
     main()
