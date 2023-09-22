0da14adbf9612f97a7f25a7ba040bc8800d5c41e
Kira Shouldis <kira.whitehat@gmail.com>
2023-09-21 21:19:15 -0400

    GitHub Training Additions & Changes

---
+++ individual_introductions/kira_shouldis.py
@@ -0,0 +1,4 @@
+def intro():
+    print('Hi! My name is Kira Shouldis. I\'m a 2nd year Electrical & Computer Engineering and Mathematics double major.' +
+          '\nI\'m brand new to AIAA and RPL, but I\'m excited to learn more about as much as I possibly can. ' +
+          '\nI\'m trying to go into offensive cybersecurity as a career, and I love talking about metal music, gaming, and tattoos! \n')
