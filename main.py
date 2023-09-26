4fed43cf644c7e55ccd843b3d45f15643d705553
Dangerkin <William Lydon>
2023-09-26 15:25:40 -0400

    GitHub Training Additions & Changes

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ sys.path.append('./individual_introductions') # or this 👉👈
 # Put all imports below this line
 from individual_introductions import shivam_patel # import the file containing your individual introduction blurb
 from individual_introductions import andrew_huth
+from individual_introductions import william_lydon
 
 def main():
     
     shivam_patel.intro() # call your intro function
     andrew_huth.intro()
+    william_lydon.intro()
+
 
 
 if __name__ == '__main__':
