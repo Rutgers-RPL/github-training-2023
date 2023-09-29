b9b51bb62c79a0a29721eeff89e3046a14398149
Kashvi Chandwani <kashvirc@gmail.com>
2023-09-28 20:06:42 -0400

    Fixed the code: got rid of apostrophes & brackets

--- individual_introductions/kashvi_chandwani.py
+++ individual_introductions/kashvi_chandwani.py
@@ -1,7 +1,7 @@
-def intro()
-{
-    print('Hello!! I am Kashvi Chandwani and I'm a sophomore studying ECE and CS! I love learning' +
-    'and meeting people, and I'm always open to new subjects and making friends. I hope to make' + 
-    'valuable contributions to the team this year, and I'm excited to see what awaits us.' +
-    'Also I love seals (the animal) so please do not hesitate to send seal pics thank u :)" )
-}
+def intro():
+    print('Hello!! I am Kashvi Chandwani and Im a sophomore studying ECE and CS! I love learning ' +
+    'and meeting people, and Im always open to new subjects and making friends. I hope to make ' + 
+    'valuable contributions to the team this year, and Im excited to see what awaits us. ' +
+    'Also I love seals (the animal) so please do not hesitate to send seal pics thank u :)' )
+
+#reminder to kashvi lol don't add apostrofuies 
